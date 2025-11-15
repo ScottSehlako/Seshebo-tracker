fe30536bc10a9f6c19b0f1969bae669e74643c9a
ScottSehlako <Scottpilgrims2001@gmail.com>
2025-11-15 19:57:13 +0200

    updated

--- lib/models/stock.dart
+++ lib/models/stock.dart
@@ -1,9 +1,10 @@
 class StockModel {
   final int? id;
-  final String type; // Meat type
+  final String type;
   final int quantityTotal;
   final int quantityRemaining;
   final double pricePerUnit;
+  final int lowStockThreshold; // Add this if you want to use it
 
   StockModel({
     this.id,
@@ -11,27 +12,28 @@ class StockModel {
     required this.quantityTotal,
     required this.quantityRemaining,
     required this.pricePerUnit,
+    this.lowStockThreshold = 0, // Default value
   });
 
-  // Convert to Map (for inserting/updating in DB)
   Map<String, dynamic> toMap() {
     return {
       'id': id,
       'meat_type': type,
       'quantity_total': quantityTotal,
       'quantity_remaining': quantityRemaining,
-      'price_per_unit': pricePerUnit,
+      'unit_price': pricePerUnit, // ← Changed to 'unit_price'
+      'low_stock_threshold': lowStockThreshold,
     };
   }
 
-  // Create StockModel from Map (DB query)
   factory StockModel.fromMap(Map<String, dynamic> map) {
     return StockModel(
       id: map['id'],
       type: map['meat_type'] ?? '',
       quantityTotal: map['quantity_total'] ?? 0,
       quantityRemaining: map['quantity_remaining'] ?? 0,
-      pricePerUnit: (map['price_per_unit'] ?? 0).toDouble(),
+      pricePerUnit: (map['unit_price'] ?? 0).toDouble(), // ← Changed to 'unit_price'
+      lowStockThreshold: map['low_stock_threshold'] ?? 0,
     );
   }
 }
